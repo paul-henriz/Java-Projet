0945323be4e5fc649269ab527c3f9ca1c04a22a9
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2019-04-27 15:29:44 +0200

    Ajout de documentation

--- Projet/src/notesElevesProfesseurs/MedianeComparator.java
+++ Projet/src/notesElevesProfesseurs/MedianeComparator.java
@@ -2,7 +2,13 @@ package notesElevesProfesseurs;
 
 import java.util.Comparator;
 
+/**
+ * Comparateur d'élève par médiane
+ */
 public class MedianeComparator implements Comparator<Eleve> {
+	/**
+	 * Surcharge pour la comparaison par médiane
+	 */
 	@Override
 	public int compare(Eleve o1, Eleve o2) {
 		if (o1.getMediane() > o2.getMediane())
