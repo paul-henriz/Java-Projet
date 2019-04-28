fc3c865990882851e263f8cd96c0685baa43331e
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2019-04-28 11:27:27 +0200

    Nettoyage

--- Projet/src/test/TestNormal.java
+++ Projet/src/test/TestNormal.java
@@ -2,7 +2,6 @@ package test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
